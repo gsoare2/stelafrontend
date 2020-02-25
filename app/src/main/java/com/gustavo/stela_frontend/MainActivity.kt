dcf8b90f875082aed50aa3d8bce5f0de57acfb20
Gustavo <gustavo.silva6785@gmail.com>
2020-02-25 09:50:22 -0300

    chat log screen

--- app/src/main/java/com/gustavo/stela_frontend/MainActivity.kt
+++ app/src/main/java/com/gustavo/stela_frontend/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.gustavo.stela_frontend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            val username = username_editext.text.toString()
+            Toast.makeText(this, username, Toast.LENGTH_LONG).show()
+            val intent = Intent(this, ChatLogActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 }
